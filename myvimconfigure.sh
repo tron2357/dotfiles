3e9db8762f71f5d44c7f417baff41cb105a536ce
canipy <canipy@gmail.com>
2014-05-30 23:30:24 +0900

    add vim configure

---
+++ myvimconfigure.sh
@@ -0,0 +1,10 @@
+./configure \
+--with-features=huge \
+--enable-multibyte \
+--enable-pythoninterp=yes \
+--enable-rubyinterp=yes \
+--enable-luainterp=yes \
+--enable-cscope \
+--enable-gpm \
+--enable-cscope \
+--enable-fail-if-missing
